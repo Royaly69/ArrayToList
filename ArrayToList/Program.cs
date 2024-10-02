8e5bcec8aa330b9a54e89862cd28279f32dd2803
iowns <iowns@ROYALY>
2024-10-02 09:15:36 +0200

    Add project files.

---
+++ ArrayToList/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("indtast Medarbejder navne på en linje, Del med Space");
+string ans = Console.ReadLine();
+string[] arr = ans.Split(" ");
+List<string> list = ArrayToList(arr);
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
+}
+
+
+static List<string> ArrayToList(string[] array)
+{
+    List<string> list = new List<string>();
+
+    foreach (string item in array)
+    {
+        list.Add(item);
+    }
+    return list;
+}
